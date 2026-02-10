ad3fae3f27fcb8ccbcf7a2adca6841717bddf8af
Erno Hentonen <erno.hentonen@opiframe.com>
2026-02-10 11:02:01 +0200

    react 1

---
+++ react/01_hello_world/src/App.tsx
@@ -0,0 +1,12 @@
+import HelloWorld from './HelloWorld';
+function App() {
+  return (
+    <>
+		<h2>Hello World</h2>
+		<HelloWorld name="Erno"/>
+		<HelloWorld />
+    </>
+  )
+}
+
+export default App
