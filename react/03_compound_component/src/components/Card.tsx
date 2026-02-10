bf065d7b2546f043c41c04fda9e8524d6e8918fc
Erno Hentonen <erno.hentonen@opiframe.com>
2026-02-10 14:05:00 +0200

    react1

---
+++ react/03_compound_component/src/components/Card.tsx
@@ -0,0 +1,41 @@
+import React,{useState} from 'react';
+import Square from './Square';
+import Label from './Label';
+
+interface State {
+	color:string;
+}
+
+const Card = () => {
+	
+	const [state,setState] = useState<State>({
+		color:"red"
+	})
+	
+	const onColorChange = () => {
+		let color = "#";
+		const colorpicker = "ABCDEF0123456789";
+		for(let i=0;i<6;i++) {
+			let temp = Math.floor(Math.random()*16);
+			color = color + colorpicker[temp];
+		}
+		setState({
+			color:color
+		})
+	}
+	
+	const cardStyle:React.CSSProperties = {
+		height:200,
+		width:150,
+		backgroundColor:"#fff",
+		filter:"drop-shadow(0px 5px 5px #666)"
+	}
+	return(
+		<div style={cardStyle}>
+			<Square color={state.color}/>
+			<Label color={state.color} onColorChange={onColorChange}/>
+		</div>
+	)
+}
+
+export default Card;
---
+++ react/03_compound_component/src/components/Label.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface Props {
+	color:string;
+	onColorChange():void;
+}
+
+const Label = (props:Props) => {
+	
+	const labelStyle:React.CSSProperties = {
+		fontFamily:"sans-serif",
+		fontWeight:"bold",
+		textAlign:"center"
+	}
+	return(
+		<p style={labelStyle} onClick={props.onColorChange}>{props.color}</p>
+	)
+}
+
+export default Label;
