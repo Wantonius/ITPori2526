4020bd8454feba89a4e629dc2af0f83420ddf162
Erno Hentonen <erno.hentonen@opiframe.com>
2026-02-11 13:08:58 +0200

    react 2

---
+++ react/06_simple_custom_hook/src/App.tsx
@@ -0,0 +1,15 @@
+import useCount from './hooks/useCount';
+
+function App() {
+
+	const [value,add,subtract] = useCount(10);
+	return (
+		<>
+			<h2>Current value:{value}</h2>
+			<button onClick={add}>+</button>
+			<button onClick={subtract}>-</button>
+		</>
+	)
+}
+
+export default App
